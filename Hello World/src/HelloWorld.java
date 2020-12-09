151e23c7d9b130f373b19999eb53bab6f8176c2a
Seulgi Park <psg081997@gmail.com>
2020-12-09 09:01:17 +0900

    c

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("world's End Dancehall");
-<<<<<<< HEAD
 		System.out.println("Earth's End Dancehall");
-=======
 		System.out.println("Hello Dance world");
->>>>>>> refs/heads/hotfix
 	}
 }
